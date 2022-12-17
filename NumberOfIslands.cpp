5349373d46ae75abad2852c1360fe700e23db283
SodamNa <78574152+SodamNa@users.noreply.github.com>
2022-12-16 19:52:59 -0800

    Update NumberOfIslands.cpp

--- NumberOfIslands.cpp
+++ NumberOfIslands.cpp
@@ -7,11 +7,11 @@ public:
             return 0; // end the function. 
 
         int num = 0; // To assign number of islands. 
-        for (int i = 0; i < grid.size(); i++) { //
+        for (int i = 0; i < grid.size(); i++) { // Execute until the end of grid.
             for (int j = 0; j < grid[0].size(); j++) {
                 if ('1' == grid[i][j]) { // If the element of grid is '1', it means an existence of island.
-                    num++; Add 1 to num.
-                    dfs(grid, i, j); To search the island(other '1' followed bt this element), execute dfs function.
+                    num++; // Add 1 to num.
+                    dfs(grid, i, j); // To search the island(other '1' followed bt this element), execute dfs function.
                 }
             }
         }
